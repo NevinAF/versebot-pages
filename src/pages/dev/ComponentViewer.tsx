89530011979a83c1c44ea3690741cf620a2909d5
Nevin Foster <nfoste2@asu.edu>
2023-04-25 11:34:32 -0700

    Init firebase

---
+++ src/pages/dev/ComponentViewer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ComponentViewer: React.FC = () => {
+	return (
+		<main>
+			<h1 className="text-2xl">
+				ComponentViewer
+			</h1>
+
+			
+		</main>
+	);
+}
+
+export default ComponentViewer;
